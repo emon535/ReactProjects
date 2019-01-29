d28648e38c63825cce9f791e2837423fb8a92480
Foysal Ahmed Emon <emon535@gmail.com>
2019-01-30 02:25:48 +0600

    Todo, Till add Todo

--- todo/src/App.js
+++ todo/src/App.js
@@ -3,23 +3,83 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      value : '',
+      list: []
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+
+  }
+
+  handleChange = (event) =>{
+    this.setState({
+      value: event.target.value
+    });
+    event.preventDefault();
+  }
+
+  handleClick = (event) =>{
+    const value = {
+      id: 1+ Math.random(),
+      value : this.state.value.slice()
+    };
+    //alert(this.state.value+' Value');
+    const list = [...this.state.list];
+    list.push(value);
+    this.setState({
+      list,
+      newItem: ""
+    });
+    event.preventDefault();
+  }
+
+  deleteItem =() =>{
+    
+  }
+  
   render() {
-    return (
+  return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2>Todo App</h2>
         </header>
+        <body>
+          <h3>Add new item </h3>
+          <form className = "inputForm">
+            <label> 
+              <input 
+                type = "text" 
+                name="todo" 
+                onChange={this.handleChange}
+              />
+              <input 
+                className="btn"
+                type = "button"
+                value = "Add"
+                onClick = {this.handleClick}
+              />
+            </label>
+           
+          </form>
+          <div>
+            <h1>List of Todo: </h1>
+            <ul>
+              {this.state.list.map(item=>{
+                return (
+                  <li key={item.id} >
+                    {item.value}
+                    <button onClick={()=>this.deleteItem(item.id)}>
+                      Remove
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </body>
       </div>
     );
   }
