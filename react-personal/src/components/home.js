b981f044882aaa5eb3aebd26afc51a0fb5b9cbdb
Foysal Ahmed Emon <foysal.ahmed@infolytx.com>
2019-02-13 16:40:00 +0600

    Project cleanup

---
+++ react-personal/src/components/home.js
@@ -0,0 +1,16 @@
+import React ,{Component} from 'react';
+
+
+
+class Home extends Component{
+
+    render(){
+        return(
+            <container>
+                <h1>Welcome</h1>
+            </container>
+        )
+    }
+}
+
+export default Home;
